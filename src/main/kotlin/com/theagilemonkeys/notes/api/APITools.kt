c6cdf7c2102b927124a6bc1d48fc6d28882ca58e
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-01-24 11:01:11 +0000

    Complete refactoring

--- src/main/kotlin/com/theagilemonkeys/notes/api/APITools.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/api/APITools.kt
@@ -2,4 +2,6 @@ package com.theagilemonkeys.notes.api
 
 import com.horizen.box.Box
 import com.horizen.box.ZenBox
-import com.horizen.box.data.ZenBoxDa
+import com.horizen.box.data.ZenBoxData
+import com.horizen.node.NodeMemoryPool
+import
