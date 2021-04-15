642b69ce6d038366066b7b02ab1baa71e05cece1
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-04-15 12:22:52 +0000

    more...

--- src/main/kotlin/com/theagilemonkeys/notes/api/requests/DeleteNoteRequest.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/api/requests/DeleteNoteRequest.kt
@@ -5,4 +5,5 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class DeleteNoteRequest(
     @JsonProperty("id") val id: String,
     @JsonProperty("proposition") override val proposition: String,
-    @JsonProperty("fee") overri
+    @JsonProperty("fee") override val fee: Long
+) : BaseRequest
