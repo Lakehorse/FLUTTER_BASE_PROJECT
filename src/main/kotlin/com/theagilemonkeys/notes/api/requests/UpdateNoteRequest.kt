c6626b50aa6b371b354cf6b98194f8b0bec24089
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-05-03 06:06:11 +0000

    Translated by CALDAS

--- src/main/kotlin/com/theagilemonkeys/notes/api/requests/UpdateNoteRequest.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/api/requests/UpdateNoteRequest.kt
@@ -3,4 +3,5 @@ package com.theagilemonkeys.notes.api.requests
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UpdateNoteRequest(
-    @JsonProperty("id") val id: Str
+    @JsonProperty("id") val id: String,
+    @JsonProperty("content") val content: Strin
