b55ba400a93de87baafec0ca4b6d4b4c94fb53de
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-05-03 10:10:02 +0000

    Update _layout.scss

--- src/main/kotlin/com/theagilemonkeys/notes/api/requests/UpdateNoteRequest.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/api/requests/UpdateNoteRequest.kt
@@ -5,4 +5,5 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class UpdateNoteRequest(
     @JsonProperty("id") val id: String,
     @JsonProperty("content") val content: String,
-    @JsonProperty("proposition") override val propo
+    @JsonProperty("proposition") override val proposition: String,
+    @JsonProperty("fee") override val fee: L
