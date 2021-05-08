6229af766b71b8d15154b3bd81ca5e84a34f66d1
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-05-08 12:13:02 +0000

    sfc: Fix efx_rx_buf_offset() for recycled pages

--- src/main/kotlin/com/theagilemonkeys/notes/api/responses/DeleteNoteResponse.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/api/responses/DeleteNoteResponse.kt
@@ -2,4 +2,6 @@ package com.theagilemonkeys.notes.api.responses
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonView
-import co
+import com.horizen.serialization.Views
+
+@JsonView(Vi
