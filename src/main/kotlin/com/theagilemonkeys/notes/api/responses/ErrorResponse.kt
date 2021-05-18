1d794d76d2e3aa6c0a3b5f415b3cf341606eb18b
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-05-18 12:16:15 +0000

    startup script

--- src/main/kotlin/com/theagilemonkeys/notes/api/responses/ErrorResponse.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/api/responses/ErrorResponse.kt
@@ -2,4 +2,5 @@ package com.theagilemonkeys.notes.api.responses
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonView
-import
+import com.horizen.api.http.ApiResponse
+import com.hor
