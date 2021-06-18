ffaf2fedbb6e24f239214562d21c609be8bd21ae
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-06-18 10:00:00 +0000

    Updated UML

--- src/main/kotlin/com/theagilemonkeys/notes/boxes/NoteBox.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/boxes/NoteBox.kt
@@ -3,4 +3,5 @@ package com.theagilemonkeys.notes.boxes
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonView
 import com.horizen.box.AbstractBox
-i
+import com.horizen.proposition.PublicKey25519Proposition
+impor
