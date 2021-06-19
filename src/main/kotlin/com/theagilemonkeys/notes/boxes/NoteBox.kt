c4d916b95331a4934603ff48fb457bd507a6db1f
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-06-19 13:28:54 +0000

    Updated links in README

--- src/main/kotlin/com/theagilemonkeys/notes/boxes/NoteBox.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/boxes/NoteBox.kt
@@ -5,4 +5,5 @@ import com.fasterxml.jackson.annotation.JsonView
 import com.horizen.box.AbstractBox
 import com.horizen.proposition.PublicKey25519Proposition
 import com.horizen.serialization.Views
-import com.thea
+import com.theagilemonkeys.notes.boxes.data.NoteBoxData
+import com.theagilem
