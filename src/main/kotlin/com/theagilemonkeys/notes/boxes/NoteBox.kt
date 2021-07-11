20a6293d54714f5a5b1bdbccf2e22b907acb69e0
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-07-11 11:12:37 +0000

    Fix broken example

--- src/main/kotlin/com/theagilemonkeys/notes/boxes/NoteBox.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/boxes/NoteBox.kt
@@ -6,4 +6,7 @@ import com.horizen.box.AbstractBox
 import com.horizen.proposition.PublicKey25519Proposition
 import com.horizen.serialization.Views
 import com.theagilemonkeys.notes.boxes.data.NoteBoxData
-import com.theagilemonkeys.notes.boxes.serializers.NoteBoxSeri
+import com.theagilemonkeys.notes.boxes.serializers.NoteBoxSerializer
+
+@JsonView(Views.Default::class)
+data cla
