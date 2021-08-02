cddb5aa1e3270f070605f5c7ba99c4a0e3c27808
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-08-02 09:56:11 +0000

    memory

--- src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
@@ -1,4 +1,6 @@
 package com.theagilemonkeys.notes.boxes.data
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.annotation.Js
+import com.fasterxml.jackson.annotation.JsonView
+import com.horizen.box.data.AbstractBoxData
+import com.hori
