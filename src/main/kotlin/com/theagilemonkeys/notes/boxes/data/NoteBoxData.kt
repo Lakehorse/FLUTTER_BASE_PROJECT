027c108419d230e7493b3f34a5de856eeac2f12f
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-08-04 18:29:53 +0000

    more statistics

--- src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
@@ -4,4 +4,5 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonView
 import com.horizen.box.data.AbstractBoxData
 import com.horizen.proposition.PublicKey25519Proposition
-im
+import com.horizen.serialization.Views
+import com.
