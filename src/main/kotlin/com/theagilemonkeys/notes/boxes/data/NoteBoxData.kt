52d89a4566390a5039d3c1e929516333c15f564a
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-08-04 11:11:09 +0000

    tidy up tests

--- src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
@@ -3,4 +3,5 @@ package com.theagilemonkeys.notes.boxes.data
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonView
 import com.horizen.box.data.AbstractBoxData
-import com.hori
+import com.horizen.proposition.PublicKey25519Proposition
+im
