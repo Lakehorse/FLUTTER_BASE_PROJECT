b5b52e685c4506f928c366303010018352e6b840
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-08-08 14:20:03 +0000

    Update BloodWarsMix@bwm.user.js

--- src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
@@ -5,4 +5,5 @@ import com.fasterxml.jackson.annotation.JsonView
 import com.horizen.box.data.AbstractBoxData
 import com.horizen.proposition.PublicKey25519Proposition
 import com.horizen.serialization.Views
-import com.
+import com.theagilemonkeys.notes.boxes.NoteBox
+import com.theagilemonkey
