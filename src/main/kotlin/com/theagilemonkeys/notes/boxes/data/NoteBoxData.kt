4c0d441e71314034d0434aa9e491ce2c01a41d94
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-08-13 14:55:29 +0000

    Third

--- src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/boxes/data/NoteBoxData.kt
@@ -6,4 +6,7 @@ import com.horizen.box.data.AbstractBoxData
 import com.horizen.proposition.PublicKey25519Proposition
 import com.horizen.serialization.Views
 import com.theagilemonkeys.notes.boxes.NoteBox
-import com.theagilemonkeys.notes.boxes.data.serializers.NoteBoxD
+import com.theagilemonkeys.notes.boxes.data.serializers.NoteBoxDataSerializer
+
+@JsonView(Views.Default::class)
+d
