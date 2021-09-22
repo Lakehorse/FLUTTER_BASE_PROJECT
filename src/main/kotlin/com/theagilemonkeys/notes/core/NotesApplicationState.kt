64996a732f4c33329b987177285e3a324437f523
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-09-22 12:31:50 +0000

    HashMap

--- src/main/kotlin/com/theagilemonkeys/notes/core/NotesApplicationState.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/core/NotesApplicationState.kt
@@ -3,3 +3,5 @@ package com.theagilemonkeys.notes.core
 import NoteCreatedValidator
 import NoteDeletedValidator
 import com.horizen.block.SidechainBlock
+import com.horizen.box.Box
+import com.horizen.proposition.P
