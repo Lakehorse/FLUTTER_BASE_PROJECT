72880983c2a5161bb57983eda017a29be942e60d
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-09-23 07:55:19 +0000

    Migrate to travis-ci.com

--- src/main/kotlin/com/theagilemonkeys/notes/core/NotesApplicationState.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/core/NotesApplicationState.kt
@@ -5,4 +5,5 @@ import NoteDeletedValidator
 import com.horizen.block.SidechainBlock
 import com.horizen.box.Box
 import com.horizen.proposition.Proposition
-import com.horizen.state.Applic
+import com.horizen.state.ApplicationState
+import com.horizen.state.Side
