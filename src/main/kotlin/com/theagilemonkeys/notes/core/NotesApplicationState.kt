e754e531e51d2ea3875e460512569677137a4c43
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-10-08 17:47:53 +0000

    New .gitignore

--- src/main/kotlin/com/theagilemonkeys/notes/core/NotesApplicationState.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/core/NotesApplicationState.kt
@@ -7,4 +7,5 @@ import com.horizen.box.Box
 import com.horizen.proposition.Proposition
 import com.horizen.state.ApplicationState
 import com.horizen.state.SidechainStateReader
-import com.horizen.transaction.Bo
+import com.horizen.transaction.BoxTransaction
+import com.theagilemonkeys.notes.extensio
