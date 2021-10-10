803e140cedcec0f306988215dab085ff8647d669
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-10-10 17:29:27 +0000

    Lots of cleanup

--- src/main/kotlin/com/theagilemonkeys/notes/core/NotesApplicationState.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/core/NotesApplicationState.kt
@@ -10,4 +10,5 @@ import com.horizen.state.SidechainStateReader
 import com.horizen.transaction.BoxTransaction
 import com.theagilemonkeys.notes.extensions.getOrThrow
 import com.theagilemonkeys.notes.transactions.NoteCreatedTransaction
-import com.theagilemo
+import com.theagilemonkeys.notes.transactions.NoteDeletedTransaction
+import com
