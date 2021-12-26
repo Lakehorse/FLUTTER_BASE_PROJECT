f9783b69e072a8c2f68fa02d2a9dd4c12019d1ff
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2021-12-26 08:14:22 +0000

    iwlwifi: fix 6000 series channel switch command

--- src/main/kotlin/com/theagilemonkeys/notes/extensions/ArrayExtensions.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/extensions/ArrayExtensions.kt
@@ -3,4 +3,5 @@ package com.theagilemonkeys.notes.extensions
 import scorex.util.serialization.Writer
 
 fun Collection<ByteArray>.serialize(writer: Writer) {
- 
+    writer.putInt(size)
+    forEach { writer.putBytes(it) 
