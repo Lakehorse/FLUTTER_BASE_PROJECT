0c8a80e4f644f8055af413a37cd587b44d54d801
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2022-01-02 22:57:20 +0000

    synx

--- src/main/kotlin/com/theagilemonkeys/notes/transactions/NoteCreatedTransaction.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/transactions/NoteCreatedTransaction.kt
@@ -4,4 +4,5 @@ import com.horizen.box.Box
 import com.horizen.box.data.BoxData
 import com.horizen.box.data.ZenBoxData
 import com.horizen.proof.Signature25519
-import co
+import com.horizen.proposition.Proposition
+import com.horizen.transac
