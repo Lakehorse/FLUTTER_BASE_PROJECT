10fa7d184d3e0762d1587e17a7b48a1df3939f48
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2022-01-05 07:00:42 +0000

    updated Slovak translation

--- src/main/kotlin/com/theagilemonkeys/notes/transactions/NoteCreatedTransaction.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/transactions/NoteCreatedTransaction.kt
@@ -5,4 +5,5 @@ import com.horizen.box.data.BoxData
 import com.horizen.box.data.ZenBoxData
 import com.horizen.proof.Signature25519
 import com.horizen.proposition.Proposition
-import com.horizen.transac
+import com.horizen.transaction.AbstractRegularTransaction
+import com.theagil
