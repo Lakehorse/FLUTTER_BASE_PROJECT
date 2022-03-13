a7e85bacacbf842970122f0139edc0ac660fe64f
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2022-03-13 12:19:57 +0000

    inicio

--- src/main/kotlin/com/theagilemonkeys/notes/transactions/NoteDeletedTransaction.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/transactions/NoteDeletedTransaction.kt
@@ -4,4 +4,5 @@ import com.horizen.box.Box
 import com.horizen.box.data.BoxData
 import com.horizen.box.data.ZenBoxData
 import com.horizen.proof.Signature25519
-import com.horizen.proposit
+import com.horizen.proposition.Proposition
+import com.horizen.transaction.Abst
