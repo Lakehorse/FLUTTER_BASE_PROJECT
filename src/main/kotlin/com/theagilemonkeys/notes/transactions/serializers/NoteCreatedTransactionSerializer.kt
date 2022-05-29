1188b7102e912ed540d7fe4ae7790d4ec377affa
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2022-05-29 21:57:28 +0000

    improved logging

--- src/main/kotlin/com/theagilemonkeys/notes/transactions/serializers/NoteCreatedTransactionSerializer.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/transactions/serializers/NoteCreatedTransactionSerializer.kt
@@ -1,4 +1,6 @@
 package com.theagilemonkeys.notes.transactions.serializers
 
 import com.horizen.transaction.TransactionSerializer
-import com.theagilemonkeys.notes.transactions.NoteCreate
+import com.theagilemonkeys.notes.transactions.NoteCreatedTransaction
+import scorex.util.serialization.Reader
+impo
