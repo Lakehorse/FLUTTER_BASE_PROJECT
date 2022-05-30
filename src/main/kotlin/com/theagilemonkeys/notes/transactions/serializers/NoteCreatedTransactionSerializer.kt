7c99d5ce96967a560ddaaf42f5048d27ec7cff4f
Lakehorse <121097819+Lakehorse@users.noreply.github.com>
2022-05-30 18:56:45 +0000

    update metadata

--- src/main/kotlin/com/theagilemonkeys/notes/transactions/serializers/NoteCreatedTransactionSerializer.kt
+++ src/main/kotlin/com/theagilemonkeys/notes/transactions/serializers/NoteCreatedTransactionSerializer.kt
@@ -3,4 +3,6 @@ package com.theagilemonkeys.notes.transactions.serializers
 import com.horizen.transaction.TransactionSerializer
 import com.theagilemonkeys.notes.transactions.NoteCreatedTransaction
 import scorex.util.serialization.Reader
-impo
+import scorex.util.serialization.Writer
+
+class NoteCreatedTransa
